e0907e99303dcca3c84ba4d506e7729038ba7062
KARIM BEN YEZZA <karim.b.yezza@gmail.com>
2021-10-16 15:53:36 +0200

    add layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+import { Tab, Tabs } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      secondary: {
+        main: "#E33E7F",
+      },
+      primary: {
+        main: "#ffffff",
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Layout></Layout>
+    </ThemeProvider>
   );
 }
+
+function Layout() {
+  return (
+    <Container>
+      <Header>
+        <HeaderNav />
+      </Header>
+      <Content />
+      <Footer />
+    </Container>
+  );
+}
+
+const Header = styled.div`
+  height: 70px;
+  width: 100%;
+  margin-left: 20px;
+  margin-right: 20px;
+  background-color: green;
+  display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
+`;
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+function HeaderNav() {
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+      <Tab label="Ranking" {...a11yProps(0)} />
+      <Tab label="Submission" {...a11yProps(1)} />
+    </Tabs>
+  );
+}
+
+const Footer = styled.div`
+  height: 100px;
+  width: 100%;
+  background-color: green;
+`;
+
+const Content = styled.div`
+  flex: 1;
+  background-color: blue;
+`;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
 
 export default App;
